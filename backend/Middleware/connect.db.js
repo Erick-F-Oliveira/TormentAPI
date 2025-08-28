2a85bf14ff23cf68cf53b926b62d23eb33e048f1
Erick Ferreira de Oliveira <63250462+Erick-F-Oliveira@users.noreply.github.com>
2025-08-28 11:52:56 +0000

    Atualiza mensagens de log e refatora importações no middleware de conexão com o MongoDB

--- backend/Middleware/connect.db.js
+++ backend/Middleware/connect.db.js
@@ -3,12 +3,12 @@ import chalk from 'chalk';
 const connect = async function connect(mongoose, uri) {
   try {
     await mongoose.connect(uri);
-    console.log(chalk.green('✅ Conexão com MongoDB estabelecida!'));
+     console.log(chalk.green('🌿 Conexão com o MongoDB estabelecida com sucesso. ✔'));
     return true; // Retorna verdadeiro se a conexão for bem-sucedida
 
 
   } catch (error) {
-    console.error(chalk.red('❌ Erro ao conectar ao MongoDB:'), error);
+    console.error(chalk.bold.red('❌ Erro ao conectar ao MongoDB:'), error);
     throw error; // Propaga o erro para quem chamou a função
   }
 }
--- backend/index.js
+++ backend/index.js
@@ -1,38 +1,32 @@
-import * as dotenv from 'dotenv';
-import express from 'express';
-import mongoose from 'mongoose';
-import cors from 'cors';
-import router from './Routes/Weapon.route.js';
-import connect from './Middleware/connect.db.js';
-import chalk from 'chalk';
-
+import * as dotenv from "dotenv";
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import router from "./Routes/Weapon.route.js";
+import Connect from "./Middleware/connect.db.js";
+import chalk from "chalk";
 dotenv.config();
-
 const app = express();
 const PORT = 5000;
-
-console.log("até aqui ok");
 
 // Middleware
 app.use(cors({ origin: "*" }));
 app.use(express.json());
 
 // Rotas
-app.use("/api", router);
-console.log("até aqui ok");
+app.use("/", router);
 
 // Conexão com MongoDB e inicialização do servidor
-const mongoUri = "mongodb+srv://Obernac:%40Mongo25181891@bots-discord.jdcllyt.mongodb.net/Bots-discord-teste?retryWrites=true&w=majority";
+const mongoUri =''
+  
 
 try {
-  await connect(mongoose, mongoUri);
-  console.log(chalk.bold.green('🌿 Conexão com o MongoDB estabelecida com sucesso.✔'));
+  await Connect(mongoose, mongoUri);
 
   app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
+    console.log(chalk.green(`💻 Servidor no ar, rodando na porta: ${PORT} ✔`));
   });
-
 } catch (error) {
   console.error("Erro ao conectar ao MongoDB:", error);
 }
-console.log("ronaldo")
+
