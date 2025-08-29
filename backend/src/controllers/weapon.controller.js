b2803eeb7fc8a4e08c74f4ed19cab2ce9adb22b2
Erick Ferreira de Oliveira <63250462+Erick-F-Oliveira@users.noreply.github.com>
2025-08-29 13:40:17 +0000

    Refatora os controladores de armas para melhorar a legibilidade e a estrutura do código. Adiciona métodos para buscar armas pelo nome e pelo operador, e atualiza as rotas para utilizar esses novos métodos.

--- backend/src/controllers/weapon.controller.js
+++ backend/src/controllers/weapon.controller.js
@@ -1,10 +1,70 @@
 import Weapon from "../models/Weapon.js";
 
+//Controller para buscar todas as armas
+const getAllWeapons = async (req, res) => {
+  try {
+    const weapon = await Weapon.find();
+    res.status(200).json(weapon);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao buscar weapon" });
+  }
+};
+
+//Controller para buscar arma pelo ID
+const getWeaoiponById = async (req, res) => {
+  const id = req.params.id;
+  try {
+    const weapon = await Weapon.findOne({ uniqueID: id });
+    res.status(200).json(weapon);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao buscar todas as armas" });
+  }
+};
+
+//Controller para buscar arma pelo nome
+const getWeaponByName = async (req, res) => {
+  const name = req.params.name;
+  try {
+    const weapon = await Weapon.findOne({ name });
+    res.status(200).json(weapon);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao buscar arma pelo nome" });
+  }
+};
+
+//Controller para buscar todas armas por quem cadastrou
+const getWeaponByOperator = async (req, res) => {
+  const username = req.params.username;
+  try {
+    const weapon = await Weapon.findOne({ operator: username });
+    res.status(200).json(weapon);
+  } catch (error) {
+    res.status(500).json({ error: "Erro ao buscar arma por cadastrante" });
+  }
+};
+
+//Controller para criar uma nova arma
 const createWeapon = async (req, res) => {
   console.log("Chegou na controller");
   try {
-    const {name, proficiency,proposite,hilt,price,baseDamage,criticalDamage,typeOfDamage,reach,space,origin,author,reference,page,operator} = req.body;
-    const sequence = (await Weapon.countDocuments()) + 1
+    const {
+      name,
+      proficiency,
+      proposite,
+      hilt,
+      price,
+      baseDamage,
+      criticalDamage,
+      typeOfDamage,
+      reach,
+      space,
+      origin,
+      author,
+      reference,
+      page,
+      operator,
+    } = req.body;
+    const sequence = (await Weapon.countDocuments()) + 1;
     const newWeapon = new Weapon({
       sequence,
       name,
@@ -21,13 +81,11 @@ const createWeapon = async (req, res) => {
       author,
       reference,
       page,
-      operator
+      operator,
     });
 
     await newWeapon.save();
     res.status(201).json(newWeapon);
-
-
   } catch (error) {
     res
       .status(500)
@@ -35,4 +93,10 @@ const createWeapon = async (req, res) => {
   }
 };
 
-export { createWeapon };
+export {
+  createWeapon,
+  getAllWeapons,
+  getWeaoiponById,
+  getWeaponByName,
+  getWeaponByOperator,
+};
--- backend/src/routes/weapon.route.js
+++ backend/src/routes/weapon.route.js
@@ -1,64 +1,24 @@
-import * as express from "express";
+import express from "express";
 import * as chalk from "chalk";
 import Weapon from "../models/Weapon.js";
 import logger from "../utils/logger.js";
-import { createWeapon } from "../controllers/weapon.controller.js";
+import {
+  createWeapon,
+  getAllWeapons,
+  getWeaoiponById,
+  getWeaponByName,
+  getWeaponByOperator,
+} from "../controllers/weapon.controller.js";
+
 const router = express.Router();
 
-// Busca todos as armas
-router.get("/weapons", async (req, res) => {
-  try {
-    const weapon = await Weapon.find();
-    res.status(200).json(weapon);
-  } catch (error) {
-    res.status(500).json({ error: "Erro ao buscar weapon" });
-    console.error(chalk.bold.red("❌ Erro ao buscar weapon:\n"), error);
-  }
-});
+//GET
+router.get("/weapons", getAllWeapons);
+router.get("/id/:id", getWeaoiponById);
+router.get("/name/:name", getWeaponByName);
+router.get("/operator/:username", getWeaponByOperator);
 
-// Busca armas pelo ID
-router.get("/id/:id", async (req, res) => {
-  const id = req.params.id;
-  try {
-    const weapon = await Weapon.findOne({ uniqueID: id });
-    res.status(200).json(weapon);
-  } catch (error) {
-    res.status(500).json({ error: "Erro ao buscar todas as armas" });
-    console.error(chalk.bold.red("❌ Erro ao buscar todas as armas:\n"), error);
-  }
-});
-
-// Busca armas pelo Nome
-router.get("/name/:name", async (req, res) => {
-  const name = req.params.name;
-  try {
-    const weapon = await Weapon.findOne({ name });
-    res.status(200).json(weapon);
-  } catch (error) {
-    res.status(500).json({ error: "Erro ao buscar arma pelo nome" });
-    console.error(chalk.bold.red("❌ Erro ao buscar arma pelo nome:\n"), error);
-  }
-});
-
-// Busca todas armas por quem cadastrou
-router.get("/operator/:username", async (req, res) => {
-  const username = req.params.username;
-  try {
-    const weapon = await Weapon.findOne({ operator: username });
-    res.status(200).json(weapon);
-  } catch (error) {
-    res.status(500).json({ error: "Erro ao buscar arma por cadastrante" });
-    console.error(
-      chalk.bold.red("❌ Erro ao buscar arma pelo nome de quem cadastrou:\n"),
-      error
-    );
-  }
-
- 
-});
-
- //POST
-
-  // Adiciona uma nova arma
+//POST
 router.post("/", createWeapon);
+
 export default router;
