e79af1383138dd2899b8460fad1295c9c8478408
Erick <erick.gcbr@gmail.com>
2025-08-31 17:25:10 -0300

    Adiciona esquema de validação para armas usando Joi

---
+++ backend/src/schemas/weapon.joi.js
@@ -0,0 +1,18 @@
+import Joi from "joi";
+
+const weaponSchema = Joi.object({
+  name: Joi.string().required().min(3),
+  proficiency: Joi.string().required(),
+  proposite: Joi.string().required(),
+  hilt: Joi.string().required(),
+  price: Joi.number().required(),
+  baseDamage: Joi.string().required(),
+  criticalDamage: Joi.string().required(),
+  typeOfDamage: Joi.string().required(),
+  reach: Joi.string().required(),
+  space: Joi.number().required(),
+  origin: Joi.string().valid("oficial", "homebrew"),
+  reference: Joi.string().required(),
+  page: Joi.number().optional(),
+});
+export default weaponSchema;
