73b0fdd8dfe3ad76b0eacf9a31b506584a161f58
Erick Ferreira de Oliveira <63250462+Erick-F-Oliveira@users.noreply.github.com>
2025-09-08 15:28:41 +0000

    remove imports desnecessarios

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,7 +1,6 @@
 import styles from "./Home.module.css";
-import api from "../../Service/axios.service";
 import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
+
 
 function Home() {
 
@@ -10,7 +9,7 @@ function Home() {
       <main className={styles.home}>
         <p>Ajude a construir a<br/> maior base de dados do <br/>maior RPG do Brasil</p>
         <div className={styles.btn}>
-        <Link to="/auth">{" "}Começar →</Link>
+        <Link to="/auth">Começar →</Link>
       </div>
       </main>
       
