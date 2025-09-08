886886324fb171ae5f1c3562bdb7e7fcfa961a20
Erick-F-Oliveira <erick.gcbr@gmail.com>
2025-09-08 15:49:11 -0300

    cria componet de card, mocka dados json de weapons

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,5 +1,23 @@
 import styles from "./Home.module.css";
 import { Link } from "react-router-dom";
+import Card from "../../components/Card";
+const sampleBook = {
+  "name": "Espada curta",
+  "proficiency": "Arma Simples",
+  "proposite": "Corpo a Corpo",
+  "hilt": "Leve",
+  "price": 10,
+  "baseDamage": "1d6",
+  "criticalDamage": "19",
+  "typeOfDamage": "Perfuração",
+  "reach": " Corpo a Corpo ",
+  "space": 1,
+  "description": "O tipo mais comum de espada...",
+  "reference": "Livro Básico JDA",
+  "page": 0,
+  "sequence": 2,
+  "operator": "obernac",
+};
 
 
 function Home() {
@@ -7,12 +25,16 @@ function Home() {
   return (
     <>
       <main className={styles.home}>
-        <p>Ajude a construir a<br/> maior base de dados do <br/>maior RPG do Brasil</p>
+        <p>Ajude a construir a<br /> maior base de dados do <br />maior RPG do Brasil</p>
         <div className={styles.btn}>
-        <Link to="/auth">Começar →</Link>
-      </div>
+          <Link to="/auth">Começar →</Link>
+        </div>
+        <div className={styles.cards}>
+          <Card book={sampleBook} />
+          <Card book={sampleBook} />
+        </div>
       </main>
-      
+
     </>
   );
 }
